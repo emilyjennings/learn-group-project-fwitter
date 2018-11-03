0879eb86aab57847aba9d66a28f49fa60a7251e6
github username <github email address>
2018-11-03 17:51:27 -0400

    made a few controller requests for tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,26 @@
 class TweetsController < ApplicationController
 
-  get '/tweets' do
+  get '/tweets/new' do
     @tweet = Tweet.all
+    erb :new
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.all
+    erb :show
+  end
+
+  get "/tweets/:id" do
+    @tweet = Tweet.find(params[:id])
+    redirect '/tweets/#{tweets.id}'
+  end
+
+  get '/tweets/:id/edit' do
+    erb :edit
+  end
+
+  post '/tweets/:id' do
+    redirect '/tweets/#{tweets.id}'
   end
 
 end
